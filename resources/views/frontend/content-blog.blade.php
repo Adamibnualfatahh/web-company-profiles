9fd46a7c97274ab582b2912b44e9f9e95344b5f6
Adamibnualfatahh <adamibnu012@gmail.com>
2022-01-05 16:24:36 +0700

    Comment Blogs Featured

--- resources/views/frontend/content-blog.blade.php
+++ resources/views/frontend/content-blog.blade.php
@@ -150,8 +150,28 @@
          
           
         </div>
+        <div id="disqus_thread" class="mt-20 mr-20 ml-20"></div>
     </section>
 
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://company-site.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
     <script src="{{ asset('js/vendor/jquery-3.5.1-min.js') }}"></script>
     <script src="{{ asset('js/vendor/modernizr-3.7.1.min.js') }}"></script>
     <script src="https://cdn.tailwindcss.com"></script>
